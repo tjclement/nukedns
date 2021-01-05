2641c1c979d1edbdaef5539c14c27ef38ea832dd
Tom <mr@tomclement.nl>
2021-01-05 17:59:58 +0100

    Added rudimentary deny list

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use trust_dns_proto::xfer::DnsResponse;
 use trust_dns_proto::rr::{DNSClass, RecordType, Record, Name};
 use trust_dns_server::authority::{MessageResponseBuilder, MessageRequest};
 use trust_dns_client::op::LowerQuery;
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufRead, BufReader};
 
 fn recurse(query: &LowerQuery) -> Vec<Record> {
     let address = "8.8.8.8:53".parse().unwrap();
@@ -29,46 +32,64 @@ fn recurse(query: &LowerQuery) -> Vec<Record> {
     return answers;
 }
 
+fn parse_denylist() -> Option<Vec<String>> {
+    let file = File::open("./denylist.txt").ok()?;
+    let reader = BufReader::new(file);
+    let mut deny_entries:Vec<String> = Vec::with_capacity(4096);
+
+    for line in reader.lines() {
+        if let Ok(domain) = line{
+            deny_entries.push(domain.trim_start_matches("||").trim_end_matches("^").to_string());
+        }
+    }
+
+    return Some(deny_entries);
+}
+
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:1053")?;
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
         let mut buf = [0u8; 1024];
-        // let mut byte_vec: Vec<u8> = Vec::with_capacity(512);
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        let buf = &mut buf[..amt];
-        io::stdout().write(&buf)?;
-        let mut decoder = BinDecoder::new(&buf);
-        let request = MessageRequest::read(&mut decoder).unwrap();
-        let answers = recurse(&request.queries()[0]);
+        let deny_entries = parse_denylist().unwrap();
 
+        loop {
+            // Receives a single datagram message on the socket. If `buf` is too small to hold
+            // the message, it will be cut off.
+            // let mut byte_vec: Vec<u8> = Vec::with_capacity(512);
+            let (amt, src) = socket.recv_from(&mut buf)?;
+            let partial_buf = &mut buf[..amt];
+            let mut decoder = BinDecoder::new(&partial_buf);
+            let request = MessageRequest::read(&mut decoder).unwrap();
 
-        let header: Header = Header::new();
-        let mut message = Message::new();
-        message
-            .set_id(request.id())
-            .set_message_type(MessageType::Response)
-            .set_op_code(OpCode::Query)
-            .set_authoritative(true)
-            .add_answers(answers)
-            .set_recursion_desired(true)
-            .set_recursion_available(true)
-            .set_authentic_data(true)
-            .set_checking_disabled(true)
-            .set_response_code(ResponseCode::NoError);
+            let header: Header = Header::new();
+            let mut message = Message::new();
+            message
+                .set_id(request.id())
+                .set_message_type(MessageType::Response)
+                .set_op_code(OpCode::Query)
+                .set_authoritative(true)
+                .set_recursion_desired(true)
+                .set_recursion_available(true)
+                .set_authentic_data(true)
+                .set_checking_disabled(true);
 
-        message.add_answer(Record::new());
-        message.add_name_server(Record::new());
-        message.add_additional(Record::new());
+            let domain = &(request.queries()[0].name().to_string().trim_end_matches(".").to_string());
+            if (deny_entries.contains(domain)) {
+                message.set_response_code(ResponseCode::NXDomain);
+            } else {
+                let answers = recurse(&request.queries()[0]);
+                message.add_answers(answers)
+                    .set_response_code(ResponseCode::NoError);
+            }
 
-        let mut byte_vec: Vec<u8> = Vec::with_capacity(512);
-        {
-            let mut encoder = BinEncoder::new(&mut byte_vec);
-            message.emit(&mut encoder).unwrap();
+            let mut byte_vec: Vec<u8> = Vec::with_capacity(512);
+            {
+                let mut encoder = BinEncoder::new(&mut byte_vec);
+                message.emit(&mut encoder).unwrap();
+            }
+
+            socket.send_to(byte_vec.as_slice(), &src)?;
         }
-
-        socket.send_to(byte_vec.as_slice(), &src)?;
     } // the socket is closed here
     Ok(())
 }
