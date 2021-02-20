661d6a770c26c88d4f659f7482ad29d99ac6e8e4
Tom <mr@tomclement.nl>
2021-02-20 19:56:13 +0100

    Added dirty implementation of answer caching with TTL-based cache invalidation, fixed answer asserting authority when it shouldn't

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use std::net::SocketAddr;
 
-use std::{io, io::Write};
-use trust_dns_proto::op::{Header, Message, MessageType, OpCode, ResponseCode};
+use std::{io};
+use std::sync::Arc;
+use std::time::SystemTime;
+use std::collections::HashMap;
+use trust_dns_proto::op::{Message, MessageType, OpCode, ResponseCode};
 use trust_dns_proto::serialize::binary::{BinDecoder, BinDecodable, BinEncodable, BinEncoder};
 use trust_dns_client::udp::UdpClientStream;
-use trust_dns_client::client::{Client, AsyncClient, ClientHandle};
-use trust_dns_proto::xfer::DnsResponse;
+use trust_dns_client::client::{AsyncClient, ClientHandle};
 use trust_dns_proto::rr::{DNSClass, RecordType, Record, Name};
-use trust_dns_server::authority::{MessageResponseBuilder, MessageRequest};
-use trust_dns_client::op::LowerQuery;
+use trust_dns_server::authority::{MessageRequest};
+use trust_dns_client::op::{LowerQuery};
 use std::fs::File;
-use std::path::Path;
 use std::io::{BufRead, BufReader};
-use tokio::sync::mpsc;
 use tokio::net::UdpSocket;
-use std::sync::Arc;
-use tokio::runtime::Runtime;
+use tokio::sync::RwLock;
+use tokio::time::Duration;
+use std::ops::Add;
 
+#[derive(Debug, Clone)]
+struct Answer {
+    expires: SystemTime,
+    records: Vec<Record>
+}
 
 async fn recurse(query: &LowerQuery) -> Option<Vec<Record>> {
     let stream = UdpClientStream::<UdpSocket>::new(([8,8,8,8], 53).into());
@@ -34,14 +40,15 @@ async fn recurse(query: &LowerQuery) -> Option<Vec<Record>> {
     return Some(answers);
 }
 
-fn parse_denylist() -> Option<Vec<String>> {
+fn parse_denylist() -> Option<HashMap<String, bool>> {
     let file = File::open("./denylist.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut deny_entries:Vec<String> = Vec::with_capacity(4096);
+    let mut deny_entries = HashMap::<String, bool>::new();
 
     for line in reader.lines() {
         if let Ok(domain) = line{
-            deny_entries.push(domain.trim_start_matches("||").trim_end_matches("^").to_string());
+            deny_entries.insert(domain.trim_start_matches("||").trim_end_matches("^").to_string(), true);
+            // deny_entries.push(domain.trim_start_matches("||").trim_end_matches("^").to_string());
         }
     }
 
@@ -50,28 +57,47 @@ fn parse_denylist() -> Option<Vec<String>> {
 
 
 
-async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Vec<u8>, deny_entries: Vec<String>) {
+async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Vec<u8>,
+                        deny_entries: Arc<HashMap<String, bool>>,
+                        query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>>) {
     let mut decoder = BinDecoder::new(&partial_buf);
     let request = MessageRequest::read(&mut decoder).unwrap();
 
-    let header: Header = Header::new();
     let mut message = Message::new();
     message
         .set_id(request.id())
         .set_message_type(MessageType::Response)
         .set_op_code(OpCode::Query)
-        .set_authoritative(true)
+        .set_authoritative(false)
         .set_recursion_desired(true)
         .set_recursion_available(true)
-        .set_authentic_data(true)
-        .set_checking_disabled(true);
+        .set_authentic_data(false)
+        .set_checking_disabled(false);
 
-    let domain = &(request.queries()[0].name().to_string().trim_end_matches(".").to_string());
-    if (deny_entries.contains(domain)) {
+    let query = request.queries()[0].to_owned();
+    let domain = query.name().to_string().trim_end_matches(".").to_string();
+    if deny_entries.contains_key(&domain) {
         message.set_response_code(ResponseCode::NXDomain);
-    } else {
-        let answers = recurse(&request.queries()[0]).await.unwrap();
-        message.add_answers(answers)
+    } else  {
+        let maybe_answers = {
+            let read_lock = query_cache.read().await;
+            read_lock.get(&(domain.clone(), query.query_type())).cloned()
+        };
+        let answers = if let Some(answer) = maybe_answers {
+            answer.records
+        } else {
+            let server_answers = recurse(&query).await.unwrap();
+            let mut write_cache = query_cache.write().await;
+            write_cache.insert((domain.clone(), query.query_type()),
+                               Answer {
+                                   expires: SystemTime::now()
+                                       .add(Duration::from_secs(server_answers[0].ttl() as u64)),
+                                   records: server_answers.clone()});
+            server_answers
+        };
+
+        message.add_query(request.queries()[0].original().to_owned())
+            .add_answers(answers)
             .set_response_code(ResponseCode::NoError);
     }
 
@@ -85,22 +111,37 @@ async fn handle_request(socket: Arc<UdpSocket>, src: SocketAddr, partial_buf: Ve
     socket.send_to(byte_vec.as_slice(), src).await.unwrap();
 }
 
+async fn cache_invalidator(query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>>) {
+    loop {
+        let now = SystemTime::now();
+        query_cache.write().await.retain(|_, answer| answer.expires > now);
+        tokio::time::sleep(Duration::from_secs(1*60)).await;
+    }
+}
+
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:53").await?;
-    let r = Arc::new(socket);
+    let socket = Arc::new(UdpSocket::bind("0.0.0.0:53").await?);
+    let query_cache: Arc<RwLock<HashMap<(String, RecordType), Answer>>> = Arc::new(RwLock::new(HashMap::new()));
 
     let mut buf = [0u8; 1024];
-    let deny_entries = parse_denylist().unwrap();
+    let deny_entries = Arc::new(parse_denylist().unwrap());
+
+    let write_cache = query_cache.clone();
+    tokio::spawn(async move {
+        cache_invalidator(write_cache).await;
+    });
 
     loop {
-        let (len, addr) = r.recv_from(&mut buf).await?;
-        let deny_list = deny_entries.to_owned();
+        let (len, addr) = socket.recv_from(&mut buf).await?;
         let partial_buf = (&mut buf[..len]).to_vec();
-        let s = r.clone();
+        let sock = socket.clone();
+        let deny = deny_entries.clone();
+        let cache = query_cache.clone();
+
         tokio::spawn(async move {
-            handle_request(s, addr, partial_buf, deny_list).await;
+            handle_request(sock, addr, partial_buf, deny, cache).await;
         });
     }
 }
